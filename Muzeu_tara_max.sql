7dde6219aa00d5755e18e2fa092302a9f12617e8
samandonesei <sam.zflx@yahoo.com>
2017-04-11 22:11:29 +0300

    Add files via upload

---
+++ Muzeu_tara_max.sql
@@ -0,0 +1,64 @@
+create or replace package best_furnizor is
+  function max_muzeu(p_id in detinator.id%type) return pls_integer;
+  procedure tara_per_muzeu(p_cursor out sys_refcursor);
+end best_furnizor;
+create or replace package body best_furnizor is
+  function max_muzeu(p_id in detinator.id%type)return pls_integer is
+  v_total pls_integer;
+  begin
+    select max(total) into v_total from
+     (select m.id,l.tara,count(a.id) total from detinator m
+      join artefact a on a.ID_DETINATOR=m.ID 
+      join LOCATIE l on l.ID=a.ID_LOCATIE
+      where m.tip='Muzeu' and m.id=p_id
+      group by m.id,m.nume,l.tara);
+      return v_total;
+  end;
+  procedure tara_per_muzeu(p_cursor out sys_refcursor) is
+    v_total_muz pls_integer;
+    v_total pls_integer;
+    no_muzeu EXCEPTION;
+    pragma exception_init(no_muzeu,-20003);
+    empty_table EXCEPTION;
+    pragma exception_init(empty_table,-20002);
+  begin
+    select count(*) into v_total_muz from detinator where tip='Muzeu';
+    select count(*) into v_total from detinator;
+    if v_total=0 then raise empty_table;
+    elsif v_total_muz=0 then raise no_muzeu;
+    else
+    open p_cursor for 
+      select m.nume,l.tara from detinator m
+        join artefact a on a.ID_DETINATOR=m.ID 
+        join LOCATIE l on l.ID=a.ID_LOCATIE
+        where m.tip='Muzeu' 
+        group by m.id,m.nume,l.tara
+        having count(a.id)=max_muzeu(m.id);
+      end if;
+  exception
+    when empty_table then
+     open p_cursor for select 'Baza de date a fost compromisa,va rugam contactati un administrator' from dual;
+    when no_muzeu then
+      open p_cursor for select 'Nu exista informatii legate de vreun muzeu in momentul actual!' from dual;
+  end;
+end best_furnizor;
+
+
+/*
+drop package best_furnizor;
+
+declare 
+  l_cursor sys_refcursor;
+  l_nume DETINATOR.NUME%type;
+  --l_int pls_integer;
+  l_tare LOCATIE.TARA%type;
+begin
+  best_furnizor.tara_per_muzeu(p_cursor=>l_cursor);
+  loop
+    fetch l_cursor
+    into l_nume,l_tare;
+    exit when l_cursor%notfound;
+    dbms_output.put_line(l_nume||' '||l_tare);
+  end loop;
+  close l_cursor;
+end;*/
