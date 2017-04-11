c5a11bb3169d262e6a1df780def708b7305d7bc9
samandonesei <sam.zflx@yahoo.com>
2017-04-11 23:35:58 +0300

    Update Muzeu_tara_max.sql

--- Muzeu_tara_max.sql
+++ Muzeu_tara_max.sql
@@ -1,3 +1,8 @@
+create index detinator on artefact(id_detinator);
+create index locatie on artefact(id_locatie);
+create index muzeu_nume on detinator (tip,id,nume);
+create index cautare on locatie(tara,oras,nume_sit);
+
 create or replace package best_furnizor is
   function max_muzeu(p_id in detinator.id%type) return pls_integer;
   procedure tara_per_muzeu(p_cursor out sys_refcursor);
@@ -7,11 +12,11 @@ create or replace package body best_furnizor is
   v_total pls_integer;
   begin
     select max(total) into v_total from
-     (select m.id,l.tara,count(a.id) total from detinator m
+     (select count(a.id) total from detinator m
       join artefact a on a.ID_DETINATOR=m.ID 
       join LOCATIE l on l.ID=a.ID_LOCATIE
       where m.tip='Muzeu' and m.id=p_id
-      group by m.id,m.nume,l.tara);
+      group by m.id,l.tara,l_oras);
       return v_total;
   end;
   procedure tara_per_muzeu(p_cursor out sys_refcursor) is
@@ -28,11 +33,11 @@ create or replace package body best_furnizor is
     elsif v_total_muz=0 then raise no_muzeu;
     else
     open p_cursor for 
-      select m.nume,l.tara from detinator m
+      select m.nume,l.tara,l_oras from detinator m
         join artefact a on a.ID_DETINATOR=m.ID 
         join LOCATIE l on l.ID=a.ID_LOCATIE
         where m.tip='Muzeu' 
-        group by m.id,m.nume,l.tara
+        group by m.id,m.nume,l.tara,l_oras
         having count(a.id)=max_muzeu(m.id);
       end if;
   exception
@@ -52,13 +57,14 @@ declare
   l_nume DETINATOR.NUME%type;
   --l_int pls_integer;
   l_tare LOCATIE.TARA%type;
+  l_oras LOCATIE.ORAS%type;
 begin
   best_furnizor.tara_per_muzeu(p_cursor=>l_cursor);
   loop
     fetch l_cursor
-    into l_nume,l_tare;
+    into l_nume,l_tare,l_oras;
     exit when l_cursor%notfound;
-    dbms_output.put_line(l_nume||' '||l_tare);
+    dbms_output.put_line(l_nume||' '||l_tare||' '||l_oras);
   end loop;
   close l_cursor;
 end;*/
