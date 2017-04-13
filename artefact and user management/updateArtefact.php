f76e0e39482f6afcdca3623ef560b8de903af1fe
NicuRoman <manijovani@yahoo.com>
2017-04-13 13:20:55 +0300

    Update updateArtefact.php

--- artefact and user management/updateArtefact.php
+++ artefact and user management/updateArtefact.php
@@ -7,18 +7,18 @@
         trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
         exit();
     }
-	$comand = 'begin
-	artefact_administration.updateArtefact(' . '\'' .$_GET['NameField']. '\'' . ','
-	.'\''.$_GET['ArheologField'].'\''.','
-	.'\''.$_GET['LocationField'].'\''.','
-	.'\''.$_GET['ColorField'].'\''.','
-	.'\''.$_GET['MaterialField'].'\''.','	
-	.'\''.$_GET['TimeField'].'\''.','	
-	.$_GET['FoundDateField'].','
-	.'\''.$_GET['RolField'].'\''.','
-	.'\''.$_GET['ValueField'].'\''.','
-	.'\''.$_GET['DetainField'].'\''.','
-	.'\''.$_GET['TypoField']. '\') ;end; ';
+	$comand = "begin
+	artefact_administration.updateArtefact('".$_GET['NameField']."',
+	'".$_GET['ArheologField']."',
+	'".$_GET['LocationField']."',
+	'".$_GET['ColorField']."',
+	'".$_GET['MaterialField']."',	
+	'".$_GET['TimeField']."',	
+	'".$_GET['FoundDateField']."',
+	'".$_GET['RolField']."',
+	'".$_GET['ValueField']."',
+	'".$_GET['DetainField']."',
+	'".$_GET['TypoField']. " ') ;end; ";
     $instruction = oci_parse($conn, $comand);
     oci_execute($instruction);
 	ECHO "ARTEFACT SUCESFULL ALTERED";
